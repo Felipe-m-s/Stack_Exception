886b5a2b6c08a10b900d0ff26372d208feac49b9
Felipe-m-s <fs33143martins@gmail.com>
2025-11-05 10:44:40 -0300

    Resolvendo atividade 1, 2 e 3 da lista de revisão

--- Main.java
+++ Main.java
@@ -1,22 +1,34 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
         Stack a = new Stack();
 
-        try {
-            a.push(10);
-            a.push(20);
-            a.push(30);
-    
-            while (!a.isEmpty()) {
-                System.out.println(a.top());
-                a.pop();
-            }
-            a.pop();
-            
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(e.getMessage());
-        }
+        //* Primeira parte
+        a.push(10);
+        a.push(20);
+        a.push(30);
 
+        Stack b = new Stack(a);
+        a.pop();    //* Removendo elemento do top de A
+        
+        System.out.println("Pilha A:");
+        a.show();
+        System.out.println("Pilha B:");
+        b.show();
+
+        //* Segunda parte
+        System.err.println("Digite a quantidade de números a serem inseridos");
+        int num = read.nextInt();
+        Stack s = new Stack(num);
+        System.err.println("Pilha: ");
+        s.show();
+
+        //* Terceira parte
+        System.err.println(Stack.equals(a, s) ? "Pilhas Iguais!" : "Pilhas diferentes!");
+
+        read.close();
     }
 }
--- Stack.java
+++ Stack.java
@@ -39,6 +39,60 @@ public class Stack {
         return this.top.item;
     }
 
+    public Stack (Stack outro) {            //* Criando pilha cópia
+        Node n = outro.top;
+        Stack s = new Stack();
+        while (n != null) {
+            s.push(n.item);
+            n = n.next;
+        }
+        this.size = 0;
+        this.top = null;
+
+        while (!s.isEmpty()) {
+            this.push(s.pop());
+        }
+    }
+
+    public Stack (int num_param) {          //* Criando um construdor para inserir n numeros
+        this.top = null;
+        this.size = 0;
+
+        for (int i = 1; i <= num_param; i++) {
+            this.push(i);
+        }
+    }
+
+    public void show() {                    //* Método para imprimir as pilhas
+        Node n = this.top;
+
+        while (n != null) {
+            System.out.println(n.item);
+            n = n.next;
+        }
+    }
+
+    //* Comparando se duas pilhas são iguais
+    public static boolean equals (Stack stack1_param, Stack stack2_param) {
+        if (stack1_param.size() != stack2_param.size()) {
+            return false;
+        }
+        else {
+            Node n1 = stack1_param.top;
+            Node n2 = stack2_param.top;
+
+            while (n1 != null) {
+                if (n1.item != n2.item) {
+                    return false;
+                }
+                n1 = n1.next;
+                n2 = n2.next;
+            }
+
+        }
+        return true;
+    }
+
 }
 
 class Node {
